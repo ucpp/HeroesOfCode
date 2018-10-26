b8dd017f5c40e8a80b2fd81aaf2d9855a89e00c8
MaryanYaroma <yaroma@panzerdog.com>
2018-10-26 10:19:17 +0200

    removed unused functions from gridHit

--- Assets/HeroesOfCode/Scripts/GridSystem/GridHit.cs
+++ Assets/HeroesOfCode/Scripts/GridSystem/GridHit.cs
@@ -8,17 +8,6 @@ namespace Maryan.HeroesOfCode
         [SerializeField]
         private Grid _grid;
 
-        public Tile.Type GetTileType(Point point)
-        {
-            return (Tile.Type)_grid.Map[point.x, point.y].Value;
-        }
-
-        public Tile.Type GetTileType(Vector2 hitPosition)
-        {
-            var point = GetPoint(hitPosition);
-            return (Tile.Type)_grid.Map[point.x, point.y].Value;
-        }
-
         public Point GetPoint(Vector2 hitPosition)
         {
             var point = _grid.GetPointByWorldPosition(hitPosition);
