684f2eaec5783023b706e82f1798255b358aa329
MaryanYaroma <yaroma@panzerdog.com>
2018-10-30 11:52:15 +0200

    added tile map editor

---
+++ Assets/HeroesOfCode/Scripts/Editor/TileMapEditor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Maryan.HeroesOfCode
+{
+    [CustomEditor(typeof(TileMapBehaviour))]
+    public class TileMapEditor : Editor
+    {
+        private TileMapBehaviour Target
+        {
+            get { return target as TileMapBehaviour; }
+        }
+
+        private void OnSceneGUI()
+        {
+            if(!Target.IsEnable)
+            {
+                return;
+            }
+
+            int controlId = GUIUtility.GetControlID(FocusType.Passive);
+            switch(Event.current.GetTypeForControl(controlId))
+            {
+                case EventType.MouseDown:
+                    GUIUtility.hotControl = controlId;
+                    if(Event.current.button == 0)
+                    {
+                        Vector3 mousePosition = Event.current.mousePosition;
+                        mousePosition.y = SceneView.currentDrawingSceneView.camera.pixelHeight - mousePosition.y;
+                        mousePosition = SceneView.currentDrawingSceneView.camera.ScreenToWorldPoint(mousePosition);
+                        var point = Target.GridHit.GetPoint(mousePosition);
+                        bool isEmptyTile = Target.Grid.Map[point.x, point.y].Value == (int)Tile.Type.Empty;
+                        Target.Grid.Map[point.x, point.y].Value = isEmptyTile ? (int)Tile.Type.Impassable : (int)Tile.Type.Empty;
+                    }
+                    Event.current.Use();
+                    break;
+                case EventType.MouseUp:
+                    //Возвращаем другим control'ам доступ к событиям мыши
+                    GUIUtility.hotControl = 0;
+                    Event.current.Use();
+                    break;
+            }
+        }
+    }
+}
--- Assets/HeroesOfCode/Scripts/GridSystem/TileMapBehaviour.cs
+++ Assets/HeroesOfCode/Scripts/GridSystem/TileMapBehaviour.cs
@@ -2,37 +2,51 @@
 
 namespace Maryan.HeroesOfCode
 {
-    public class GlobalGridGizmo : MonoBehaviour
+    public class TileMapBehaviour : MonoBehaviour
     {
+        public bool IsEnable
+        {
+            get { return _isEnableEdit; }
+        }
+        public GridHit GridHit
+        {
+            get { return _gridHit; }
+        }
+        public Grid Grid
+        {
+            get { return _grid; }
+        }
         [SerializeField]
-        private bool _enabled;
+        private bool _isEnableEdit;
+        [SerializeField]
+        private Grid _grid;
+        [SerializeField]
+        private GridHit _gridHit;
         [SerializeField]
         private Color _gridColor = Color.yellow;
         [SerializeField]
         private Color _impassableTilecolor = Color.red;
-        [SerializeField]
-        private Grid _grid;
 
         private void OnDrawGizmos()
         {
-            if(_enabled)
+            if(_isEnableEdit)
             {
                 Gizmos.color = _gridColor;
 
                 float h = _grid.TileHeight * _grid.Height;
                 float w = _grid.TileWidth * _grid.Width;
-
+                Vector2 startPos = _grid.StartPosition;
                 for(int y = 0; y <= _grid.Height; y++)
                 {
-                    Vector3 start = new Vector3(_grid.StartPosition.x, _grid.StartPosition.y - y * _grid.TileHeight, 0);
-                    Vector3 end = new Vector3(_grid.StartPosition.x + w, _grid.StartPosition.y - y * _grid.TileHeight, 0);
+                    Vector3 start = new Vector3(startPos.x, startPos.y - y * _grid.TileHeight, 0);
+                    Vector3 end = new Vector3(startPos.x + w, startPos.y - y * _grid.TileHeight, 0);
                     Gizmos.DrawLine(start, end);
                 }
 
                 for(int x = 0; x <= _grid.Width; x++)
                 {
-                    Vector3 start = new Vector3(_grid.StartPosition.x + x * _grid.TileWidth, _grid.StartPosition.y, 0);
-                    Vector3 end = new Vector3(_grid.StartPosition.x + x * _grid.TileWidth, _grid.StartPosition.y - h, 0);
+                    Vector3 start = new Vector3(startPos.x + x * _grid.TileWidth, startPos.y, 0);
+                    Vector3 end = new Vector3(startPos.x + x * _grid.TileWidth, startPos.y - h, 0);
                     Gizmos.DrawLine(start, end);
                 }
 
@@ -45,8 +59,8 @@ namespace Maryan.HeroesOfCode
                         if((Tile.Type)_grid.Map[x, y].Value == Tile.Type.Impassable)
                         {
                             Vector3 size = new Vector3(_grid.TileWidth, _grid.TileHeight, 0.1f);
-                            float dx = _grid.StartPosition.x + x * _grid.TileWidth + _grid.TileWidth / 2;
-                            float dy = _grid.StartPosition.y - y * _grid.TileHeight - _grid.TileHeight / 2;
+                            float dx = startPos.x + x * _grid.TileWidth + _grid.TileWidth / 2;
+                            float dy = startPos.y - y * _grid.TileHeight - _grid.TileHeight / 2;
                             Gizmos.DrawCube(new Vector3(dx, dy, -1), size);
                         }
                     }
